f324e3480cf65efe28697dd47f73868edcd8cf60
CherokiDev <miguelangelmorato84@gmail.com>
2023-06-19 18:59:20 +0200

    added cors and remote database

--- src/app.js
+++ src/app.js
@@ -1,9 +1,12 @@
+import 'dotenv/config';
 import express from "express";
 import playerRoutes from "./routes/player.routes.js";
 import scheduleRoutes from "./routes/schedule.routes.js";
+import cors from './middleware/cors.js';
 
 const app = express();
 
+app.use(cors);
 app.use(express.json());
 
 app.use(playerRoutes);
