3f40b4d0b13896ea95a820d4b1201a2ebd073c33
CherokiDev <miguelangelmorato84@gmail.com>
2024-05-23 18:28:31 +0200

    feat: Add CORS configuration for frontend host
    This commit adds CORS configuration to allow requests from the frontend host specified in the environment variable. It uses the `cors` package to handle CORS headers and options. The configuration includes the allowed origin, methods, headers, and credentials.

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import "dotenv/config";
 import dotenv from "dotenv";
 import express from "express";
+import cors from "cors";
 import playerRoutes from "./routes/player.routes.js";
 import scheduleRoutes from "./routes/schedule.routes.js";
 import "./models/associations.js";
@@ -9,24 +10,16 @@ dotenv.config();
 
 const app = express();
 
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", `${process.env.FRONTEND_HOST}`);
-  res.header("Access-Control-Allow-Credentials", "true");
-  res.header(
-    "Access-Control-Allow-Methods",
-    "GET,HEAD,OPTIONS,POST,PUT,DELETE"
-  );
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-  );
+// Configuración de CORS
+const corsOptions = {
+  origin: process.env.FRONTEND_HOST, // o puedes usar un array de strings para permitir múltiples dominios
+  methods: "GET,HEAD,OPTIONS,POST,PUT,DELETE",
+  allowedHeaders:
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization",
+  credentials: true, // permite cookies
+};
 
-  if (req.method === "OPTIONS") {
-    return res.status(200).end();
-  }
-
-  next();
-});
+app.use(cors(corsOptions));
 
 app.use(express.json());
 
